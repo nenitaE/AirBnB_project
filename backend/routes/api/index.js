134d84ffde2322bbe2e319d4a77a68e0845dbd84
NenitaE <nenitaespinosa@gmail.com>
2023-03-13 14:36:08 -0400

    Phase 4: Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,46 +1,19 @@
-// backend/routes/api/index.js
 const router = require('express').Router();
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-// TESTING PHASE 3
-//const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Worldly1'
-//       }
-//     });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
+router.use('/session', sessionRouter);
 
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+router.use('/users', usersRouter);
 
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
