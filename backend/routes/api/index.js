687ff2d470e2e57c086e8ca32467688ca8dc2419
NenitaE <nenitaespinosa@gmail.com>
2023-03-13 19:51:14 -0400

    Phase:5 Refactored users table to include firstName and lastName columns

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -12,8 +12,8 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
