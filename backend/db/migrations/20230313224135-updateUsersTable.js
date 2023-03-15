1cb109376420c35b3f03dc3f8bad8ec654ae836d
NenitaE <nenitaespinosa@gmail.com>
2023-03-14 20:13:21 -0400

    options object added to migration

--- backend/db/migrations/20230313224135-updateUsersTable.js
+++ backend/db/migrations/20230313224135-updateUsersTable.js
@@ -1,18 +1,25 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', "firstName", {
-      type: Sequelize.STRING
+  async up(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    await queryInterface.addColumn(options, "firstName", {
+      type: Sequelize.STRING(30)
     });
-    await queryInterface.addColumn('Users', "lastName", {
-      type: Sequelize.STRING,
+    await queryInterface.addColumn(options, "lastName", {
+      type: Sequelize.STRING(30)
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', "firstName");
-    await queryInterface.removeColumn('Users', "lastName");
+    options.tableName = "Users";
+    await queryInterface.removeColumn(options, "firstName");
+    await queryInterface.removeColumn(options, "lastName");
   }
 };
