b51c77ea310219d2b1c3a65b4af2c55e20319a9d
NenitaE <nenitaespinosa@gmail.com>
2023-04-29 18:48:57 -0400

    signup modal completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -20,17 +20,17 @@ const removeUser = () => {
 
 
 export const login = (user) => async (dispatch) => {
-const { credential, password } = user;
-const response = await csrfFetch('/api/session', {
-    method: 'POST',
-    body: JSON.stringify({
-    credential,
-    password,
-    }),
-});
-const data = await response.json();
-dispatch(setUser(data.user));
-return response;
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+      method: 'POST',
+      body: JSON.stringify({
+      credential,
+      password,
+      }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
 };  
 
 
